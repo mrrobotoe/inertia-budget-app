ba93d17ce16654adf7e936b422006b5316e404aa
mrrobotoe <aldo.martinez@my.utsa.edu>
2025-01-03 15:57:19 -0600

    added breadcrumb to header for reflect page

--- resources/js/Pages/Budgets/Reflect.tsx
+++ resources/js/Pages/Budgets/Reflect.tsx
@@ -1,4 +1,12 @@
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbSeparator,
+} from '@/Components/ui/breadcrumb';
 import { SidebarTrigger } from '@/Components/ui/sidebar';
+
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 export default function Reflect() {
     return (
@@ -8,7 +16,28 @@ export default function Reflect() {
                 <div className="flex items-center gap-2">
                     <SidebarTrigger className="-ml-1" />
                     <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                        Reflect
+                        <Breadcrumb>
+                            <BreadcrumbList>
+                                <BreadcrumbItem>
+                                    <BreadcrumbLink
+                                        href={route(
+                                            'budgets.show',
+                                            route().params.budget,
+                                        )}
+                                    >
+                                        <h1>Budget</h1>
+                                    </BreadcrumbLink>
+                                </BreadcrumbItem>
+                                <BreadcrumbSeparator />
+                                <BreadcrumbItem>
+                                    <BreadcrumbLink
+                                        href={route().current()?.split('.')[1]}
+                                    >
+                                        <h1>Relfect</h1>
+                                    </BreadcrumbLink>
+                                </BreadcrumbItem>
+                            </BreadcrumbList>
+                        </Breadcrumb>
                     </h2>
                 </div>
             }
